3fac8a15e9ce7c7c03019883fb2a14fa2a061812
alanrichard77 <alanrichard_@outlook.com>
2025-07-27 15:19:37 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import ScannerTable from '@/components/ScannerTable';
-
-const Index = () => {
-  return <ScannerTable />;
-};
-
-export default Index;
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
